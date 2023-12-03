7a0b52f6eae96f75d7520032ca73ab0e8db38caa
AyAztuB <ayaztub@gmail.com>
2023-12-03 15:17:22 -0800

    update template with useful functions

--- 2023_ocaml/dayX/dayX.ml
+++ 2023_ocaml/dayX/dayX.ml
@@ -1,5 +1,17 @@
 let file = "input"
 
+let (~++) x = x+1
+let (~--) x = x-1
+let (>>>) x y = if x > y then x else y
+let (<<<) x y = if x < y then x else y
+
+let get_input f =
+    let rec _get_line out =
+        match input_line f with
+        line -> _get_line (line :: out)
+        | exception End_of_file -> List.rev out |> Array.of_list
+    in _get_line []
+
 let solve f =
     let rec _solve part1 part2 =
         match input_line f with
@@ -11,4 +23,5 @@ let () =
     let f = open_in file in
     let res = solve f in
     close_in f;
-    print_endline (Format.sprintf "part1: %d\npart2: %d" (fst res) (snd res))
+    "part1: " ^ (fst res |> string_of_int) ^ "\npart2: " ^ (snd res |> string_of_int)
+    |> print_endline
